7be94aa7cdf68c88f55a7ee6ccc3a35299ffd11f
Jaime Ernesto Perla Romero <jaimeperla26@gmail.com>
2024-03-02 20:13:30 -0600

    se avanzo 2.4

--- js/reservas.js
+++ js/reservas.js
@@ -36,29 +36,4 @@ $('#btn').click(function(){
       
     })
     
-    var i = 0;
-    var txt = 'Contacto';
-    var speed = 100;
-    
-    function typeWriter() {
-      if (i < txt.length) {
-        document.getElementById("title").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
-        
-      }
-    }
-    
-    
-    
-    
-    oal.addEventListener('submit', function(event) {
-      event.preventDefault();
-    
-      
-      const correo = document.getElementById("email");
-      const correoValor = correo.value;
-    
-      oal.reset();
-    });
-    
+   
